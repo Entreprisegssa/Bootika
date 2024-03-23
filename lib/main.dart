48be9ee4466553832ab2b0fd67e15316e0263884
Freeman Lovelace <florentarnaza@gmail.com>
2024-03-22 21:14:08 -0400

    connexion a la BD etablie

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,59 @@
+import 'dart:developer';
+
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_database/firebase_database.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'firebase_options.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  if(kIsWeb){
-  await Firebase.initializeApp(options: const FirebaseOptions(
-    apiKey: "AIzaSyCdrJ1ako9M6qkELkxwGw-jIt1lM7SxgeI" , 
-    appId: "1:667656625654:web:0467f1df5a02d966dc7bd7" , 
-    messagingSenderId: "667656625654", 
-    projectId: "bootika-eb4fe" ));
-  }
-// await Firebase.initializeApp(
-//     options: DefaultFirebaseOptions.currentPlatform,
-// );
-  runApp(const MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final db = FirebaseFirestore.instance;
+  void addUserToDatabase() {
+    Map<String, dynamic> user = {
+        'name' : 'Freeman Lovelace',
+        'age'  : '23',
+        'city' : 'yakro'
+      };
+    db.collection('users').add(user).then((DocumentReference doc) => 
+        log("Document added with id ${doc.id}"));
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      title: 'Bootika',
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Si vous clickez sur le boutton suivant,'
+              ),
+               const Text(
+                'un document sera ajouter a la base de donnees'
+              ),
+               const SizedBox(height: 10.0,),
+               const Text(
+                'Ceci est fait dans le but de tester la connexion'
+              ),
+              ElevatedButton(
+                onPressed: addUserToDatabase,
+                child: const Text("SEND DATA"),
+              ),
+            ],
+          )
+        ),
       ),
-      home: const Formulaire(),
     );
   }
 }
-class Formulaire extends StatefulWidget {
-  const Formulaire({super.key});
-
-  @override
-  State<Formulaire> createState() => _FormulaireState();
-}
-
-class _FormulaireState extends State<Formulaire> {
-
-final GlobalKey<FormState> _key = GlobalKey<FormState>();
-final TextEditingController _id = TextEditingController();
-final TextEditingController _mdp = TextEditingController();
-
-
-@override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-   DatabaseReference _databaseReference = FirebaseDatabase.instance.ref().child("user");
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
-
